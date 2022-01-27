6601f5a6b3ac7b8735bb3bc5e03172baa5e6f6b9
anujmedjaii <anuj.sharma@coforge.com>
2022-01-27 19:19:52 +0530

    bug fixed in storybook component

--- storybook/stories/ListView/ListView.stories.js
+++ storybook/stories/ListView/ListView.stories.js
@@ -7,12 +7,30 @@ import ListTile from '../../../src/ListTile';
 import CenterView from '../CenterView';
 storiesOf('ListView', module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
-  .add('News Tile', () => (
+  .add('Normal News Tile', () => (
     <ListTile
       author={'Anuj'}
       image={
         'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
       }
+      headline={'Headline for the news'}
+      desc={'we have a huge discussion'}
+    />
+  ))
+  .add('News Tile Without Headline', () => (
+    <ListTile
+      author={'Anuj'}
+      image={
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+      }
+      headline={''}
+      desc={'we have a huge discussion'}
+    />
+  ))
+  .add('News Tile Without Image', () => (
+    <ListTile
+      author={'Anuj'}
+      image={''}
       headline={'Headline for the news'}
       desc={'we have a huge discussion'}
     />
